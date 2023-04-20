802f7d70b2977846758651d305885980786508a4
samxx <S.haidar@SSC-Security.net>
2023-04-20 06:31:53 +0300

    Implement a simple DropDown menu with plain css & add seperators between menu items using css siblings selector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, {useState} from "react";
+import { ReactComponent as  ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as  BoltIcon } from "./icons/bolt.svg";
+import { ReactComponent as  BellIcon } from "./icons/bell.svg";
+import { ReactComponent as  MessengerIcon } from "./icons/messenger.svg";
+import {ReactComponent as CarretIcon } from "./icons/caret.svg";
+import {ReactComponent as CogIcon } from "./icons/cog.svg";
+import {ReactComponent as ChevronIcon } from "./icons/chevron.svg";
 
-function App() {
+
+
+let NavBar = (props) =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className='navbar'>
+      <ul className='navbar-nav'>
+        {props.children}
+      </ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href = '#' className ="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children }
+    </li>
+  );
+}
+
+function Menu(props) {
+
+  function MenuItem (props) {
+    return (
+      
+      <a href ='#' className="menu-item">
+        <span className="icon-button"> {props.leftIcon} </span>
+        <span className="menu-title"> {props.children} </span> 
+        <span className="right-icon"> {props.rightIcon} </span>
+      </a>
+    );
+    
+  }
+  return (
+    <div className="dropdown">
+        <MenuItem> Profile </MenuItem>
+        <MenuItem leftIcon = {<CogIcon/> } rightIcon = {<BoltIcon/>}> Settings </MenuItem>
+        <MenuItem> Quit </MenuItem>
+
     </div>
   );
 }
+function App() {
+  return (
+    <NavBar>
+      <NavItem icon = {<ArrowIcon/>}/>
+      <NavItem icon = {<BoltIcon/>}/>
+      <NavItem icon = {<MessengerIcon/>}/>
+      <NavItem icon = {<BellIcon/>}/>
+      <NavItem icon = {<CarretIcon/>}> 
+        <Menu/>
+      </NavItem>
+
+
+    </NavBar>
+  );
+}
+
 
 export default App;
